0c84f910312cd429f395620fccaac74584c53acd
cinnamondev <justpromptiguess@gmail.com>
2024-02-20 19:22:33 +0000

    pulses square demo code, need to fix distances.

--- src/main.cpp
+++ src/main.cpp
@@ -7,36 +7,23 @@
 
 #include "mbed.h"
 #include "Sequencer.hpp"
+#include "QEI.hpp"
+#include "../c12832/C12832.h"
 #include "stm32f4xx.h"
-/** Blinking rate (ms) */
-#define BLINKING_RATE 600ms
 
-void portInit(void) {
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  /*Configure GPIO pin : PC4 */
-  GPIO_InitTypeDef gpiob67;
-  gpiob67.Pin = GPIO_PIN_6 | GPIO_PIN_7;
-  gpiob67.Alternate = GPIO_AF2_TIM4;
-  gpiob67.Mode = GPIO_MODE_INPUT;
-  gpiob67.Pull = GPIO_PULLUP;
+EventQueue q;
 
-  HAL_GPIO_Init(GPIOB, &gpiob67);
-}
 int main() {
-  //__HAL_RCC_TIM3_CLK_ENABLE();
-  //portInit();
-  //Ticker quadratureUpdate;
-
-  //  Quadrature encoderL(TIM3);
-  //  Quadrature encoderR(TIM4);
-  //quadratureUpdate.attach(callback([&encoderR]() {
-  //                            encoderR.tick();
-  //                          }), 10ms);
+  C12832 display(NC,NC,NC,NC,NC);
   QEI encoderR(PB_13, PB_14, NC, 255);
   encoderR.reset();
-  // d5 d9 quadrature tim3
-  //Motor motorL(D1, D2, &encoderL);
-  Motor motorR(D9, NC, &encoderR);
-  Sequencer sequencer(&motorR,&motorR,std::vector<Movement*> {new Line(1000), new Line(1000)});
+  Motor motorR(D9, D8, &encoderR);
+  Sequencer sequencer(&motorR,&motorR,std::vector<Movement*> {new Line(1000), new Turn(90), new Line(1000), new Turn(90), new Line(1000), new Turn(180)});
+  sequencer.repeats(true);
   sequencer.play();
+  while(true) {
+    display.cls();
+    display.locate(10,10);
+    display.printf("Pulses: %d", encoderR.getPulses());
+  }
 }
