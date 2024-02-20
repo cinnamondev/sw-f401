1792a243ed7c821c01eb6ccd2ef5faec94c190e9
cinnamondev <justpromptiguess@gmail.com>
2024-02-20 18:33:38 +0000

    minimal PWM output encoder demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,41 @@
 /**
  * @file main.cpp
- * @author your name (you@domain.com)
- * @brief 
+ * @author cinnamondev
  * @date 2023-10-29
+ *
+ * Technical Demo A program Task 3/4
  */
 
 #include "mbed.h"
-#include "Sequencer.hpp"
-#include "stm32f4xx.h"
-/** Blinking rate (ms) */
-#define BLINKING_RATE 600ms
+#include "../c12832/C12832.h"
 
-void portInit(void) {
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  /*Configure GPIO pin : PC4 */
-  GPIO_InitTypeDef gpiob67;
-  gpiob67.Pin = GPIO_PIN_6 | GPIO_PIN_7;
-  gpiob67.Alternate = GPIO_AF2_TIM4;
-  gpiob67.Mode = GPIO_MODE_INPUT;
-  gpiob67.Pull = GPIO_PULLUP;
+//PwmOut motorL(PB_13);
+PwmOut motorR(PB_14);
 
-  HAL_GPIO_Init(GPIOB, &gpiob67);
-}
+InterruptIn enableSwitch(A2);
+InterruptIn disableSwitch(A3);
 int main() {
-  //__HAL_RCC_TIM3_CLK_ENABLE();
-  //portInit();
-  //Ticker quadratureUpdate;
+  auto* enable = new DigitalOut(A4, 0);
+  AnalogIn potL(A0);
+  AnalogIn potR(A1);
 
-  //  Quadrature encoderL(TIM3);
-  //  Quadrature encoderR(TIM4);
-  //quadratureUpdate.attach(callback([&encoderR]() {
-  //                            encoderR.tick();
-  //                          }), 10ms);
-  QEI encoderR(PB_13, PB_14, NC, 255);
-  encoderR.reset();
-  // d5 d9 quadrature tim3
-  //Motor motorL(D1, D2, &encoderL);
-  Motor motorR(D9, NC, &encoderR);
-  Sequencer sequencer(&motorR,&motorR,std::vector<Movement*> {new Line(1000), new Line(1000)});
-  sequencer.play();
+  motorR.period_us(20);
+  motorR = 0.5;
+  //QEI encoderR(PB_13, PB_14, NC, 255);
+  enableSwitch.fall(callback([enable]() {
+    enable->write(0);
+  }));
+  disableSwitch.fall(callback([enable]() {
+    enable->write(1);
+  }));
+  float outL = 0.5; float outR = 0.5;
+  while(true) {
+    outL = potL.read();
+    outR = potR.read();
+    if ((0.4 < outL) & (outL < 0.6)) { outL = 0.5; }
+    if ((0.4 < outR) & (outR < 0.6)) { outR = 0.5; }
+    //motorL = outL;
+    motorR = outR;
+
+  }
 }
