fe178c4c379f0148b2bd4146ecaacb1aaa5e7f9c
cinnamondev <justpromptiguess@gmail.com>
2024-02-21 19:08:22 +0000

    cmd fix ssitch case

--- src/Bluetooth/Bluetooth.cpp
+++ src/Bluetooth/Bluetooth.cpp
@@ -3,11 +3,13 @@
 Bluetooth::Bluetooth(PinName tx, PinName rx, PinName ledPin) : hm10(tx,rx,9600), led(ledPin) {}
 
 void Bluetooth::checkData() {
-        if (hm10.readable()) {
-            cmd = hm10.read(&cmd, 1);
-            if (cmd == 'A') {
-                beepLED();
-            }
+            if (hm10.read(&cmd,1) == EAGAIN) { return; }
+            switch (cmd) {
+            case 'A':
+              beepLED();
+              break;
+            default:
+              break;
         }
     }
 
