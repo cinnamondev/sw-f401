1b50ec2d53b624cdf790619d853da95450f4475c
cinnamondev <justpromptiguess@gmail.com>
2024-02-28 09:21:15 +0000

    .

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,11 @@
  */
 
 #include "mbed.h"
+#include "QEI.hpp"
+#include "C12832.h"
+
+DigitalOut ledPWML(D8);
+DigitalOut ledPWMR(D5);
 
 //PwmOut motorL(PA_8);
 PwmOut motorL(PB_1);
@@ -15,12 +20,17 @@ PwmOut motorR(PC_7);
 InterruptIn enableSwitch(A2);
 InterruptIn disableSwitch(A3);
 int main() {
+  C12832 d(D11, D13, D12, D7, D10);
+  d.cls();
   auto *enable = new DigitalOut(PB_2, 0);
   AnalogIn potL(A0);
   AnalogIn potR(A1);
-
-  motorL.period_us(20);
-  motorR.period_us(20);
+  QEI encoderL(PB_12, PB_7, NC, 255, QEI::X4_ENCODING);
+  QEI encoderR(PC_3, PC_2, NC, 255, QEI::X4_ENCODING);
+  int pl1 = encoderL.getPulses();
+  int pr1 = encoderR.getPulses();
+  motorL.period_us(30);
+  motorR.period_us(30);
   motorL=0.5;
   motorR = 0.5;
   // QEI encoderR(PB_13, PB_14, NC, 255);
@@ -39,5 +49,17 @@ int main() {
     }
      motorL = outL;
     motorR = outR;
+  }
+  int pl2 = encoderL.getPulses();
+  int pr2 = encoderR.getPulses();
+  if (abs(pl2-pl1) > 10) {
+    ledPWML.write(1);
+  } else {
+    ledPWML.write(0);
+  }
+  if (abs(pr2-pr1) > 10) {
+    ledPWMR.write(1);
+  } else {
+    ledPWMR.write(0);
   }
 }
