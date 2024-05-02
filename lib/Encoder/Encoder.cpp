78a2b9ac5e04d70f0008b50f1ee97ac70457499b
cinnamondev <justpromptiguess@gmail.com>
2024-05-02 03:37:24 +0100

    encoder final version + some library files

--- lib/Encoder/Encoder.cpp
+++ lib/Encoder/Encoder.cpp
@@ -2,90 +2,40 @@
 
 #include "stm32f4xx.h"
 #include "stm32f4xx_hal_tim.h"
-#include "stm32f4xx_hal_gpio.h"
-
-#include "main.h"
-#include "ll_init.h"
-
-Encoder::Encoder(TIM_TypeDef* timer) {
-  if (timer == TIM3) {
-      MX_TIM3_Init(); // Quadrature Encoder timers
-      hwTimer = &htim3;
-  } else if (timer == TIM4) {
-      MX_TIM4_Init();
-      hwTimer = &htim4;
-  }
-  us_ticker_init(); // mbed hal api used for precision timekeeping
-
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-
-  // stm32 timer initialization
-  hwTimer->Instance = timer;
-  hwTimer->Init.Period = 0xFFFFFFFF;
-  hwTimer->Init.Prescaler = 0;
-  hwTimer->Init.ClockDivision = 0;
-  hwTimer->Init.CounterMode = TIM_COUNTERMODE_UP;
-
-  // configure timer in ENCODER mode. Ensure that the timer is not already running
-  // and is valid.
-  TIM_Encoder_InitTypeDef encoderCfg;
-  encoderCfg.EncoderMode = TIM_ENCODERMODE_TI12; // x4 encoder
-
-  encoderCfg.IC1Polarity = TIM_ICPOLARITY_RISING;
-  encoderCfg.IC1Prescaler = TIM_ICPSC_DIV1;
-  encoderCfg.IC1Selection = TIM_ICSELECTION_DIRECTTI;
-  encoderCfg.IC1Prescaler = TIM_ICPSC_DIV1;
-  encoderCfg.IC1Filter = 0;
-
-  encoderCfg.IC2Polarity = TIM_ICPOLARITY_RISING; 
-  encoderCfg.IC1Prescaler = TIM_ICPSC_DIV1;
-  encoderCfg.IC2Selection = TIM_ICSELECTION_INDIRECTTI;
-  encoderCfg.IC2Prescaler = TIM_ICPSC_DIV1;
-  encoderCfg.IC2Filter = 0;
-
-  if (HAL_TIM_Encoder_Init(hwTimer, &encoderCfg) != HAL_OK) {
-    printf("Timer is not valid");
-    fflush(stdout);
-  }
-
-  HAL_TIM_Encoder_Start(hwTimer, TIM_CHANNEL_ALL);
-
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-
-  HAL_TIMEx_MasterConfigSynchronization(hwTimer, &sMasterConfig);
+#ifndef LIB_ENCODER_NOMBED
+Encoder::Encoder(TIM_HandleTypeDef* timer, std::chrono::microseconds sampleRate) {
+  us_ticker_init(); // ensure microsecond ticker is enabled
+  tickRate = sampleRate;
+#else
+Encoder::Encoder(TIM_HandleTypeDef* timer) {
+#endif
+  hwTimer = timer;
 }
 
 void Encoder::update() {
-  static uint32_t t1 = 0;
-  static uint32_t c1 = 0;
-  uint32_t c2 = __HAL_TIM_GetCounter(hwTimer);
+#ifndef LIB_ENCODER_NOMBED
   uint32_t t2 = us_ticker_read();
-  timeDelta = (t2 >= t1) ? t2-t1 : t1-t2; // current time - last time
+#endif
+  uint32_t c2 = __HAL_TIM_GetCounter(hwTimer);
+  steps = (c2>=c1) ? c2-c1 : -1 *(c1-c2); // direction sensitive change
+#ifndef LIB_ENCODER_NOMBED
+  timeDelta = (t2-t1) ? t2-t1 : t1-t2; // prevent weird delta from OF/UF.
   t1 = t2;
-  stepDelta = (c2>c1) ? c2-c1 : c1-c2; // current counter - last counter
-  stepAcc += stepDelta;
-  c2 = c1;
-
-
-  if ((cbDistance != nullptr) && (stepAcc >= pulse_threshold)) {
-    cbDistance();
-    stepAcc = 0;
-    if (!event_repeatable) {
-      cbDistance = nullptr;
-    }
-  }
+#endif
+  c1 = c2;
 }
-void Encoder::onDistance(int distance, Callback<void()> e, bool repeat) {
-  pulse_threshold = distance;
-  cbDistance = e;
-  event_repeatable = repeat;
+#ifndef LIB_ENCODER_NOMBED
+void Encoder::start() {
+  ticker.attach(callback(this, &Encoder::update), tickRate);
+  update();
+
 }
 
-// TODO
+void Encoder::stop() {
+  ticker.detach();
+}
+#endif
 
-float Encoder::degreesToSteps(int steps, unsigned int cpr) { return 0; }
-float Encoder::distanceToSteps(float distance, float wheelDiameter,
-                               unsigned int cpr) {
-  return 0;
+int Encoder::getSteps() {
+  return steps;
 }
--- lib/Encoder/Encoder.hpp
+++ lib/Encoder/Encoder.hpp
@@ -1,28 +1,88 @@
 #ifndef SW_F401_ENCODER_HPP
 #define SW_F401_ENCODER_HPP
 
+#ifndef LIB_ENCODER_NOMBED
 #include "mbed.h"
+#endif
+
 #include "stm32f4xx.h"
 #include "stm32f4xx_hal_tim.h"
 
-class Encoder {
+/**
+ * @brief STM32F4 HAL TIM Encoder Wrapper intended for MBED OS.
+ *
+ * @section Usage (TIcker/MBED)
+ * 
+ * 1. Use STM32CubeMX to generate the timer configuration code. (Refer to your micro's reference manual and CubeMX)
+ * 2. Put the config code somewhere in your code. You need to call your MX_TIMx_Init code before using Encoder.
+ * This includes putting the Timer MSP weak functions somewhere, for the HAL to use.
+ * 3. Pass your TIM_HandleTypeDef you want to use to Encoder when initializing it.
+ * 4. Start the Encoder using `start` when ready. There are 2 ways to obtain the speed, one is more reliable
+ * while the other might hinder performance slightly. If you are able to be relatively optimistic that the
+ * internal ticker can run regularly without large delays that change its time interval, use `getSteps`,
+ * as it will be equivalent to the number of steps within `sampleRate`. Otherwise, use `getSpeed`, it will
+ * it will return a speed in terms of `steps/millisecond` and uses the mbed microsecond ticker to get a time
+ * stamp.
+ * 
+ * @section Usage (no ticker / standard codebase compatibility)
+ * 
+ * 0. Define the macro LIB_ENCODER_NOMBED to disable ticker/remove mbed dependency.
+ * 
+ * See above up to & including step 3.
+ * 
+ * 4. Regularly call `update` yourself. You must be able to call this regularly
+ * somehow, or precisely determine the time between calls, as each call updates
+ * the steps as the number of steps since last update call. If you dont have
+ * mbed/use this feature, you will not be able to make use of `getSpeed`. 
+ */
+class Encoder
+{
+  #define LIB_ENCODER_NOMBED
 public:
-  explicit Encoder(TIM_TypeDef* timer);
-  float getSpeed() const { return float(stepDelta)/float(timeDelta*1e6); }
-  unsigned int getSteps() { return stepAcc; stepAcc = 0; }
-  void update();
-  void onDistance(int distance, Callback<void()> e, bool repeat = false);
-  static float degreesToSteps(int steps, unsigned int cpr);
-  static float distanceToSteps(float distance, float wheelDiameter, unsigned int cpr);
-private:
-  TIM_HandleTypeDef* hwTimer;
-  volatile uint32_t stepAcc = 0;
-  volatile unsigned int stepDelta = 0;
-  volatile uint64_t timeDelta = 0;
-  unsigned int pulse_threshold = 0;
-  bool event_repeatable = false;
-  Callback<void()> cbDistance;
-};
+#ifndef LIB_ENCODER_NOMBED
+  /**
+   * @brief Construct a new Encoder object
+   *
+   * @param timer STM32 timer to use (see `Usage`.)
+   */
+  explicit Encoder(TIM_HandleTypeDef *timer, std::chrono::microseconds sampleRate);
+#else
+  explicit Encoder(TIM_HandleTypeDef* timer);
+#endif
+  /**
+   * @brief Returns the number of steps since the previous 'tick'.
+   *
+   * @return unsigned int Number of steps since last tick.
+   */
+  int getSteps();
+  /**
+   * @brief Start the encoder ticker at the previously defined sample rate.
+   */
+#ifndef LIB_ENCODER_NOMBED
+  void start();
+  /**
+   * @brief Stop the encoder ticker.
+   */
+  void stop();
 
+private:
+#endif
+  /**
+   * @brief Check for a change in steps counted from the timer. Call regularly (perhaps via a ticker or background event)
+   * for a regular speed.
+   *
+   */
+  void update();
+#ifndef LIB_ENCODER_NOMBED
+  Ticker ticker; /**< Mbed ticker to update encoder*/
+  std::chrono::microseconds tickRate; /**< Rate to update encoders at*/
+  uint32_t t1 = 0; /**< Previous time sample (mbed) */
+#else
+  private:
+#endif
+  TIM_HandleTypeDef *hwTimer; /**< STM32 HAL Timer Peripheral*/
+  volatile int steps = 0; /**< Number of steps since last tick*/
+  uint32_t c1 = 0;
+};
 
 #endif // SW_F401_ENCODER_HPP
