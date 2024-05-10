7b8da8135750c0bf7be75832885c2f923d7364c7
cinnamondev <justpromptiguess@gmail.com>
2024-05-10 16:28:32 +0100

    motor

--- lib/Motor/Motor.cpp
+++ lib/Motor/Motor.cpp
@@ -1,5 +1,25 @@
 #include "Motor.hpp"
 
-void Motor::onCompute(float v) {
+Motor::Motor(Motor::Config config, TIM_HandleTypeDef* tim) : PID(config.pid, 1ms, false), pwmOut(config.pwm), speed(0), encoder(tim,1ms){
+  setProcess(&speed);
+  pwmOut.period_us(30);
+  pwmOut = 0.5;
+  encoder.start();
+}
 
+void Motor::onCompute(float v) {
+  pwmOut = v;
+}
+
+void Motor::preCompute() {
+  speed = encoder.getSpeed();
+}
+
+
+void Motor::setSpeed(float stepsPTick) {
+  config.sp = stepsPTick;
+  arm_pid_reset_f32(&pid);
+}
+void Motor::setConfig(PID::Config _config) {
+  setGains(_config.kp, _config.ki, _config.kd);
 }
--- lib/Motor/Motor.hpp
+++ lib/Motor/Motor.hpp
@@ -2,20 +2,34 @@
 #define MOTOR_HPP
 
 #include "PID.hpp"
+#include "Encoder.hpp"
 
-class Motor : public PID {
+/**
+ * @brief Mbed + STSTM32 Encoder + Motor PID Controller
+ * 
+ */
+class Motor : protected PID {
 public:
-  Motor(Motor::Config);
-  Motor(PinName pwm, PinName current, PID::Config pidConfig);
+  /**
+   * @brief Motor pinout + PID configuration
+   * 
+   */
   struct Config {
     PinName pwm;
     PinName current;
-    PID::Config pidCfg;
+    PID::Config pid;
   };
-private:void onCompute(float) override;
+  Motor(Motor::Config config, TIM_HandleTypeDef* tim);
+  void setSpeed(float stepsPTick);
+  /** Update gains PID algorithm */
+  using PID::setGains;
+  void setConfig(PID::Config) override;
+  Encoder encoder;
+private:
+    void onCompute(float) override;
     void preCompute(void) override;
-    AnalogIn current;
-
+    PwmOut pwmOut;
+    float speed = 0;
 };
 
 #endif // MOTOR_HPP
